78df4d88f11af02fadd897f5bf3adc27831fc910
BenjmnG <Benjmng@gmail.com>
2021-04-07 15:33:49 +0200

    Recherche

---
+++ prototypes/art/ui/p08/index.php
@@ -0,0 +1,125 @@
+<html>
+ 
+<head>
+	<meta charset="utf-8"> 
+	<title>Clod</title>
+	<link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon"> 
+	<link rel="stylesheet" type="text/css" href="assets/css-compiled/main.css">
+	<!--Type-->
+	<link rel="stylesheet" type="text/css" href="assets/font/font-1/stylesheet.css">
+</head>
+
+<body class="bgw motion">
+	<main>
+		<ul id="nav">
+			<li class="cat show" data-color="w">
+				<h1>Abstractions</h1>
+				<ul>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+				</ul>
+			</li>
+			<li class="cat" data-color="w">
+				<h1>Instantanés</h1>
+				<ul>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+				</ul>
+			</li>
+			<li class="cat"  data-color="k">
+				<h1>Vidéos</h1>
+				<ul>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+					<li class="item">
+						<img class="cover" src="assets/svg/example.svg">
+						<!-- Place more content -->
+					</li>
+				</ul>
+			</li>
+		</ul>			
+	</main>
+	<script src="assets/js/tools.js" type="text/javascript"></script>
+	<script src="assets/js/global.js" type="text/javascript"></script>
+</body>
+
+</html>
+
