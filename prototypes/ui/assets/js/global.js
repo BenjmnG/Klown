e676c27d4fac8752b9682a94a5d572cd0279082a
BenjmnG <Benjmng@gmail.com>
2021-06-17 11:39:40 +0200

    Clean JS

--- prototypes/ui/assets/js/global.js
+++ prototypes/ui/assets/js/global.js
@@ -1,34 +1,7 @@
-body = document.querySelector('body');
-toggleSideNav = document.querySelector('#toggle-sideNav');
-sideNav = document.querySelector('#sideNav');
-
-/* 
-		Side 
-					*/
-
-/*let cats = [...document.querySelectorAll('.cat')] 
-cats.forEach( function(el, i){
-
-	el.addEventListener("mouseover", function(e) {
-		// Set Show as focus
-		if(!el.classList.contains("show")){
-			document.querySelector(".cat.show").classList.remove("show")
-			el.classList.add("show")
-		}
-	});
-
-});
-
-let els = [...document.querySelectorAll('.cat .item')]
-els.forEach( function(el, i){
-	el.addEventListener("click", function(e) {
-		// Change color
-		removeClassByPrefix(body, 'bg')
-		body.classList.add('bg' + el.closest('.cat').getAttribute("data-color"));
-	});
-});
-*/
-
+let body = document.querySelector('body'), 
+		id = body.id,
+		toggleSideNav = document.querySelector('#toggle-sideNav'),
+		sideNav = document.querySelector('#sideNav');
 
 /* 
 		Strobos 
