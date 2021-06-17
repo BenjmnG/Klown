e962b3a1725c98d8d1ca31193a4f99f5e277fb6e
BenjmnG <Benjmng@gmail.com>
2021-06-17 11:33:42 +0200

    Clean JS

--- prototypes/ui/assets/js/_cat.js
+++ prototypes/ui/assets/js/_cat.js
@@ -1,30 +1,5 @@
 let cat = body.getAttribute("data-for");
 let Width = window.screen.width;
-/*
-	Catégorie
-				*/
-/*const mySiema = new Siema({
-  selector: '#sideNav .item-container:first-of-type',
-  duration: 500,
-  easing: 'ease-in-out',
-  perPage: 5,
-  startIndex: 0,
-  draggable: true,
-  multipleDrag: true,
-  threshold: 20,
-  loop: true,
-  rtl: false,
-  onInit: () => {
-    sideNav.classList.add('siema')
-  },
-  onChange: () => {},
-});
-
-window.addEventListener("resize", function(){
-  mySiema.resizeHandler()
-});*/
-
-
 
 let scrollContainer =  document.querySelector('.cat-container'),
     scrollTrigger = document.querySelector('#scrollTrigger'),
@@ -59,39 +34,3 @@ sortOptions.forEach( function(el, i){
     }
   });
 });
-
-
-
-/*
-  Slider
-          */
-
-
-/*const delta = 0;
-let startX;
-let startY;
-let scrollLeft;
-
-const slider = document.querySelector('.cat-container ');
-
-slider.addEventListener('mousedown', function (event) {
-  startX = event.pageX || event.touches[0].pageX - slider.offsetLeft;
-  startY = event.pageY;
-
-  slider.classList.add('active');
-  scrollLeft = slider.scrollLeft;   
-});
-
-slider.addEventListener('mouseup', function (event) {
-  event.preventDefault();
-  const x = event.pageX || event.touches[0].pageX - slider.offsetLeft;
-  const diffX = Math.abs(x - startX);
-  console.log(x)
-  if (diffX < delta) {
-    // Click!
-  } else {
-    const dist = (x - startX);
-    slider.scrollLeft = scrollLeft - dist;
-  }
-  slider.classList.remove('active');
-});*/
