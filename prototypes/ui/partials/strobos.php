1fd3d91478fa2e626f5ebe460e0cb0ba0569bee1
BenjmnG <Benjmng@gmail.com>
2021-06-18 16:22:22 +0200

    Strobos

--- prototypes/ui/partials/strobos.php
+++ prototypes/ui/partials/strobos.php
@@ -1,16 +1,18 @@
 <!-- Toggle visuals affects -->
 
 <div id="strobos">
-	<label for="i-strobos">Strobos</label>
 	<input type="checkbox" class="on-off" id="i-strobos">
+	<label for="i-strobos">
+		<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+			<title>Strobos</title>
+			<g stroke="var(--cc)" fill="none">
+				<circle cx="10" cy="10" r="9" vector-effect="non-scaling-stroke"/>
+				<circle class="second" cx="10" cy="10" r="6.5" vector-effect="non-scaling-stroke"/>
+				<circle class="second" cx="10" cy="10" r="4" vector-effect="non-scaling-stroke"/>
+				<circle class="second" cx="10" cy="10" r="2" vector-effect="non-scaling-stroke"/>
+			</g>
+		</svg>
+	</label>
 </div>
 
-<svg id="circles" class="sr-only" width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-	<rect x="0" y="0" width="100%" height="100%" fill="var(--bg)" stroke="none"/>
-	<g stroke="var(--cc)" fill="none">
-		<circle cx="20" cy="20" r="18.5" />
-		<circle cx="20" cy="20" r="14.5" />
-		<circle cx="20" cy="20" r="10.5" />
-		<circle cx="20" cy="20" r="6.5" />
-	</g>
-</svg>
+
