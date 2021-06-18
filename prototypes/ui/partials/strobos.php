38b42a3195157babdae351c92924c8963aebc6b3
BenjmnG <Benjmng@gmail.com>
2021-06-18 16:26:38 +0200

    Update strobos.php

--- prototypes/ui/partials/strobos.php
+++ prototypes/ui/partials/strobos.php
@@ -1,7 +1,7 @@
 <!-- Toggle visuals affects -->
 
 <div id="strobos">
-	<input type="checkbox" class="on-off" id="i-strobos">
+	<input type="checkbox" class="on-off" id="i-strobos" checked>
 	<label for="i-strobos">
 		<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 			<title>Strobos</title>
